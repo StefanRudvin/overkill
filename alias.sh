a79f20a0545b976b6b95c2acc8a7c63ef29eac14
rudi <stefan.rudvin@hotmail.com>
2020-04-11 21:24:29 +0100

    Use docker for confluent kafka

---
+++ alias.sh
@@ -0,0 +1,9 @@
+# Add aliases to a new ubuntu installation
+
+echo 'alias l="ls"' >> ~/.bashrc
+echo 'alias c="clear"' >> ~/.bashrc
+echo 'alias overkill="cd /media/sf_overkill/"' >> ~/.bashrc
+echo 'alias home="cd ~/"' >> ~/.bashrc
+echo 'alias showbash="less ~/.bashrc"' >> ~/.bashrc
+echo 'alias bashconfig="nano ~/.bashrc"' >> ~/.bashrc
+echo 'alias s="source ~/.bashrc"' >> ~/.bashrc
